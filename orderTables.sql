4ba24c9a55a26e18761a931f853314edb973d331
rayveneja <rayvenejenningsallen@gmail.com>
2023-11-30 03:05:58 -0500

    Update orderTables.sql

--- orderTables.sql
+++ orderTables.sql
@@ -9,11 +9,10 @@ CREATE TABLE Orders (
   orderTotal DECIMAL(10, 2),
   orderTime TIMESTAMP,
   pickupTime TIMESTAMP,
-  isCancelled BOOLEAN,
-  isCompleted BOOLEAN,
-  isAcknowledged BOOLEAN,
-  orderClosedTime TIMESTAMP,
-  orderAcknowledgedTime TIMESTAMP,
+  statusO VARCHAR(50),
+  cancelTime TIMESTAMP,
+  acceptedTime TIMESTAMP,
+  escalatedTime TIMESTAMP,
   staffID INT,
   FOREIGN KEY (customerID) REFERENCES Customers(customerID),
   FOREIGN KEY (staffID) REFERENCES Staff(staffID)
@@ -23,20 +22,90 @@ CREATE TABLE Customers (
   customerID INT PRIMARY KEY,
   customerFirstName VARCHAR(50),
   customerLastName VARCHAR(50),
-  customerMiddleName VARCHAR(50)
+  customerMiddleName VARCHAR(50).
+  customerEmail VARCHAR(255),
+  customerNumber VARCHAR(15)
 );
 
 CREATE TABLE Staff (
   staffID INT PRIMARY KEY,
   staffFirstName VARCHAR(50),
   staffLastName VARCHAR(50),
-  staffMiddleName VARCHAR(50)
+  staffMiddleName VARCHAR(50),
+  staffEmail VARCHAR(255),
+  staffNumber VARCHAR(15)
 );
 
-CREATE TABLE OrderAlerts (
-  alertID INT PRIMARY KEY,
-  orderID INT,
-  alertTime TIMESTAMP,
-  isHandled BOOLEAN,
-  FOREIGN KEY (orderID) REFERENCES Orders(orderID)
-);
+
+CREATE VIEW OrderView AS
+SELECT
+  o.orderID AS OrderNumber,
+  CONCAT(c.customerFirstName, ' ', c.customerLastName) AS CustomerFullName,
+  o.orderDetails AS OrderDetails,
+  o.orderTotal AS OrderTotal,
+  o.pickupTime AS OrderPickupTime,
+  o.statusO AS OrderStatus
+FROM
+  Orders o
+  JOIN Customers c ON o.customerID = c.customerID;
+
+
+CREATE VIEW NewOrders AS
+SELECT
+  o.orderID AS OrderNumber,
+  CONCAT(c.customerFirstName, ' ', c.customerLastName) AS CustomerFullName,
+  o.orderDetails AS OrderDetails,
+  o.orderTotal AS OrderTotal,
+  o.pickupTime AS OrderPickupTime,
+  o.statusO AS OrderStatus
+FROM
+  Orders o
+  JOIN Customers c ON o.customerID = c.customerID
+WHERE
+  o.statusO = 'Pending';
+
+
+CREATE VIEW AcceptedOrders AS
+SELECT
+  o.orderID AS OrderNumber,
+  CONCAT(c.customerFirstName, ' ', c.customerLastName) AS CustomerFullName,
+  o.orderDetails AS OrderDetails,
+  o.orderTotal AS OrderTotal,
+  o.pickupTime AS OrderPickupTime,
+  o.statusO AS OrderStatus
+FROM
+  Orders o
+  JOIN Customers c ON o.customerID = c.customerID
+WHERE
+  o.statusO = 'Accepted';
+
+
+CREATE VIEW CancelledOrders AS
+SELECT
+  o.orderID AS OrderNumber,
+  CONCAT(c.customerFirstName, ' ', c.customerLastName) AS CustomerFullName,
+  o.orderDetails AS OrderDetails,
+  o.orderTotal AS OrderTotal,
+  o.pickupTime AS OrderPickupTime,
+  o.statusO AS OrderStatus
+FROM
+  Orders o
+  JOIN Customers c ON o.customerID = c.customerID
+WHERE
+  o.statusO = 'Cancelled';
+
+
+CREATE VIEW EscalatedOrders AS
+SELECT
+  o.orderID AS OrderNumber,
+  CONCAT(c.customerFirstName, ' ', c.customerLastName) AS CustomerFullName,
+  o.orderDetails AS OrderDetails,
+  o.orderTotal AS OrderTotal,
+  o.pickupTime AS OrderPickupTime,
+  o.statusO AS OrderStatus
+FROM
+  Orders o
+  JOIN Customers c ON o.customerID = c.customerID
+WHERE
+  o.statusO = 'Escalated';
+
