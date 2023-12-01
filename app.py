41778002da12498cb36203181701ddd33851b98f
RomarieHaughton <151781029+RomarieHaughton@users.noreply.github.com>
2023-11-30 21:41:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 Mystery = Flask(__name__)
 CORS(Mystery)
 
-# get customers
+# get all bookings
 
 @Mystery.route('/bookings', methods=['GET'])
 def get_bookings():
@@ -35,6 +35,38 @@ def get_bookings():
     except Exception as e:
         print(e)
         return make_response(jsonify({'error': 'An error occurred while retrieving this view'}), 500)
+
+
+
+# get bookings with a status available
+@Mystery.route('/available-bookings', methods=['GET'])
+def get_available_bookings():
+    try:
+        # connect to database
+        con = mysql.connector.connect(user='root',
+                                       host='localhost',
+                                       database='mysteryRest'
+                                       )
+        cursor = con.cursor()
+        cursor.execute(f"SELECT EventSpaces.EventName, EventCustomer.CustomerName, Bookings.EventDateTime, Bookings.EventDuration FROM Bookings JOIN EventSpaces ON Bookings.EventSpaceID = 
+        EventSpaces.EventSpaceID JOIN EventCustomer ON Bookings.CustomerID = EventCustomer.CustomerID WHERE Bookings.BookingStatus = 'available';")
+        available_booking_list = []
+        for row in cursor.fetchall():
+            booking = {
+                'EventName': row[0],
+                'CustomerName': row[1],
+                'EventDateTime': row[2],
+                'EventDuration': row[3],
+            }
+            available_booking_list.append(booking)
+        cursor.close()
+        con.close()
+        return make_response(jsonify(available_booking_list), 200)
+
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({'error': 'An error occurred while retrieving available bookings'}), 500)
+
 
 
 
