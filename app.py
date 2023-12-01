43792a53ba3034f3a5b5a5ee83236235d55a8871
RomarieHaughton <151781029+RomarieHaughton@users.noreply.github.com>
2023-11-30 21:58:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,48 @@ def get_bookings_by_status(status):
         return make_response(jsonify({'error': 'An error occurred while retrieving bookings by status'}), 500)
 
 
+# get bookings within a date range
+@Mystery.route('/bookings-by-date-range', methods=['GET'])
+def get_bookings_by_date_range():
+    try:
+        # connect to database
+        con = mysql.connector.connect(user='root',
+                                       host='localhost',
+                                       database='mysteryRest'
+                                       )
+        cursor = con.cursor()
+
+        # Get date range parameters from the request
+        start_date_str = request.args.get('start_date')
+        end_date_str = request.args.get('end_date')
+
+        # Convert date strings to datetime objects
+        start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
+        end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+
+      # Fetch bookings within the date range
+              cursor.execute("SELECT EventSpaces.EventName, EventCustomer.CustomerName, Bookings.EventDateTime, Bookings.EventDuration FROM Bookings JOIN EventSpaces ON Bookings.EventSpaceID 
+              = EventSpaces.EventSpaceID JOIN EventCustomer ON Bookings.CustomerID = EventCustomer.CustomerID WHERE Bookings.EventDateTime BETWEEN %s AND %s;", (start_date, end_date))
+
+              bookings_in_range_list = []
+              for row in cursor.fetchall():
+                  booking = {
+                      'EventName': row[0],
+                      'CustomerName': row[1],
+                      'EventDateTime': row[2],
+                      'EventDuration': row[3],
+                  }
+                  bookings_in_range_list.append(booking)
+
+              cursor.close()
+              con.close()
+              return make_response(jsonify(bookings_in_range_list), 200)
+
+          except Exception as e:
+              print(e)
+              return make_response(jsonify({'error': 'An error occurred while retrieving bookings by date range'}), 500)
+
+
 
 
 
