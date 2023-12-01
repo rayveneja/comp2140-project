2b8c4e9328a379036b9fbdc24ad8adb55c731eaf
RomarieHaughton <151781029+RomarieHaughton@users.noreply.github.com>
2023-11-30 21:46:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,44 @@ def get_available_bookings():
         return make_response(jsonify({'error': 'An error occurred while retrieving available bookings'}), 500)
 
 
+# get bookings by status
+@Mystery.route('/bookings-by-status/<status>', methods=['GET'])
+def get_bookings_by_status(status):
+    try:
+        # connect to database
+        con = mysql.connector.connect(user='root',
+                                       host='localhost',
+                                       database='mysteryRest'
+                                       )
+        cursor = con.cursor()
+
+        # Validate status to prevent SQL injection
+        if status.lower() not in ['approve', 'reject']:
+            return make_response(jsonify({'error': 'Invalid status provided'}), 400)
+
+        cursor.execute(f"SELECT EventSpaces.EventName, EventCustomer.CustomerName, Bookings.EventDateTime, Bookings.EventDuration FROM Bookings JOIN EventSpaces ON Bookings.EventSpaceID = 
+        EventSpaces.EventSpaceID JOIN EventCustomer ON Bookings.CustomerID = EventCustomer.CustomerID WHERE Bookings.BookingStatus = %s;", (status,))
+        bookings_by_status_list = []
+
+        for row in cursor.fetchall():
+            booking = {
+                'EventName': row[0],
+                'CustomerName': row[1],
+                'EventDateTime': row[2],
+                'EventDuration': row[3],
+            }
+            bookings_by_status_list.append(booking)
+
+        cursor.close()
+        con.close()
+        return make_response(jsonify(bookings_by_status_list), 200)
+
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({'error': 'An error occurred while retrieving bookings by status'}), 500)
+
+
+
 
 
 if __name__ == '__main__':
