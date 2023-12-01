b9ebf1d681dc4f4338630bc7e8addfd7b4a28337
Darien McIntosh <145591708+DarienMcIntosh@users.noreply.github.com>
2023-11-30 21:24:13 -0500

    Updated login.js

--- login.js
+++ login.js
@@ -6,7 +6,15 @@ function attemptLogin() {
     // Simple validation for demonstration
     if (username === "user123" && password === "password") {
         loginMessage.style.color = "#28a745";
-        loginMessage.textContent = "Login successful!";
+        loginMessage.textContent = "Login successful! Redirecting to the Dashboard...";
+
+        // Save authentication status (you may use more secure methods in a real application)
+        localStorage.setItem('authenticated', 'true');
+
+        // Redirect to the dashboard after a short delay
+        setTimeout(() => {
+            window.location.href = 'dashboard.html';
+        }, 1500);
     } else {
         loginMessage.style.color = "#dc3545";
         loginMessage.textContent = "Invalid username or password. Please try again.";
